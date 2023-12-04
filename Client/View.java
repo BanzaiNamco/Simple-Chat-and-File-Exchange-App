aea5c99c251b0efcad8362b16ceef6b4df534f6b
Robert Joachim Encinas <robert_joachim_encinas@dlsu.edu.ph>
2023-12-04 15:27:36 +0800

    Added labels

--- Client/View.java
+++ Client/View.java
@@ -5,6 +5,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
+
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
@@ -16,24 +18,54 @@ public class View extends JFrame {
     private JLabel serverAddress;
     private JLabel serverPort;
     private JLabel username;
+    private JPanel leavePanel;
+    private JButton leaveButton;
 
-    //Middle panel and its components
-    private JPanel mainDisplayPanel;
+    //Left panel and its components
     private JPanel messagesPanel;
     private JPanel chatlogPanel;
-    private JPanel serverlogPanel;
     private JPanel announcementlogPanel;
+    private JPanel mainChats;
+    private JPanel mainAnnouncements;
+    private final JLabel chatLabel = new JLabel("Chat");
+    private final JLabel announcementLabel = new JLabel("Announcement");
     private JScrollPane chatlogScrollable;
-    private JScrollPane serverlogScrollable;
     private JScrollPane announcementlogScrollable;
     private ArrayList<JLabel> chatMessages;
     private ArrayList<JLabel> announcementMessages;
+
+    //Right panel and its components
+    private JPanel rightPanel;
+    private JPanel serverlogPanel;
+    private JPanel mainServerLog;
+    private JPanel mainCommands;
+    private final JLabel serverLabel = new JLabel("Server Log");
+    private final JLabel commandsLabel = new JLabel("Commands");
+    private JScrollPane serverlogScrollable;
     private ArrayList<JLabel> logMessages;
+    private JPanel commandsPanel;
+    private JScrollPane commandsScrollable;
+    private ArrayList<JLabel> commands;
+
+    //Middle panel and its components
+    private JPanel mainDisplayPanel;
+    //For default
+    private JTextField host;
+    private JTextField ip;
+    private JPanel middlebuttonPanel;
+    //For directory
+    private JPanel userDirPanel;
+    private JPanel serverDirPanel;
+    private String selected;
+    private JPanel uploadPanel;
+    private JPanel downloadPanel;
+    private JButton downloadButton;
+    private JButton uploadButton;
 
     //Bottom panel
     private JPanel bottomPanel;
-    private JPanel buttonPanel;
     private JTextField commandline;
+    private JPanel sendPanel;
     private JButton sendButton;
 
     public View() {
@@ -41,7 +73,7 @@ public class View extends JFrame {
         this.setTitle("CSNETWK MP");
         this.setLayout(new BorderLayout());
         this.setResizable(false);
-        this.setSize(720, 720);
+        this.setSize(1080, 720);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         //Initialize the view
@@ -71,46 +103,87 @@ public class View extends JFrame {
 
         //Initialize the middle panels
         this.mainDisplayPanel = new JPanel();
+        this.rightPanel = new JPanel();
         this.messagesPanel = new JPanel();
         this.chatlogPanel = new JPanel();
         this.serverlogPanel = new JPanel();
         this.announcementlogPanel = new JPanel();
+        this.announcementlogPanel = new JPanel();
+        this.mainChats = new JPanel();
+        this.mainAnnouncements = new JPanel();
+        this.mainServerLog  = new JPanel();
+        this.mainCommands  = new JPanel();
 
         //Initialize log handlers
         this.chatMessages = new ArrayList<JLabel>();
         this.logMessages = new ArrayList<JLabel>();
         this.announcementMessages = new ArrayList<JLabel>();
+ 
+        //Set the default commands
+        this.commands = new ArrayList<JLabel>();
+        this.commandsPanel = new JPanel();
+        this.commands.add(new JLabel("/join <server ip>"));
+        this.commands.add(new JLabel("/?"));
+        this.commandsPanel.add(this.commands.get(0));
+        this.commandsPanel.add(this.commands.get(1));
 
         //Initialize the scrollable panes
         this.chatlogScrollable = new JScrollPane(this.chatlogPanel);
         this.serverlogScrollable = new JScrollPane(this.serverlogPanel);
         this.announcementlogScrollable = new JScrollPane(this.announcementlogPanel);
+        this.commandsScrollable = new JScrollPane(this.commandsPanel);
         
         //Set layout of messages panel
         this.messagesPanel.setLayout(new GridLayout(2, 1));
+        //Set layout of right panel
+        this.rightPanel.setLayout(new GridLayout(2, 1));
 
         //Set layout of the scrollables
         this.chatlogPanel.setLayout(new BoxLayout(this.chatlogPanel, BoxLayout.Y_AXIS));
         this.serverlogPanel.setLayout(new BoxLayout(this.serverlogPanel, BoxLayout.Y_AXIS));
         this.announcementlogPanel.setLayout(new BoxLayout(this.announcementlogPanel, BoxLayout.Y_AXIS));
+        this.commandsPanel.setLayout(new BoxLayout(this.commandsPanel, BoxLayout.Y_AXIS));
 
-        //Add to the messages panel
-        this.messagesPanel.add(this.announcementlogScrollable);
-        this.messagesPanel.add(this.chatlogScrollable);
+        //Set layout of the containers
+        this.mainChats.setLayout(new BoxLayout(this.mainChats, BoxLayout.Y_AXIS));
+        this.mainAnnouncements.setLayout(new BoxLayout(this.mainAnnouncements, BoxLayout.Y_AXIS));
+        this.mainServerLog.setLayout(new BoxLayout(this.mainServerLog, BoxLayout.Y_AXIS));
+        this.mainCommands.setLayout(new BoxLayout(this.mainCommands, BoxLayout.Y_AXIS));
 
-        //Set the layout of the main display panel
-        this.mainDisplayPanel.setLayout(new GridLayout(1, 2));
+        //Add elements to the mains
+        this.mainChats.add(this.chatLabel);
+        this.mainAnnouncements.add(this.announcementLabel);
+        this.mainServerLog.add(this.commandsLabel);
+        this.mainCommands.add(this.serverLabel);
 
-        //Add the scrollables to the main panel
-        this.mainDisplayPanel.add(this.messagesPanel);
-        this.mainDisplayPanel.add(this.serverlogScrollable);
+        //Add scrollable panels to the mains
+        this.mainChats.add(this.chatlogScrollable);
+        this.mainAnnouncements.add(this.announcementlogScrollable);
+        this.mainServerLog.add(this.serverlogScrollable);
+        this.mainCommands.add(this.commandsScrollable);
+
+        //Add to the left panel
+        this.messagesPanel.add(this.mainChats);
+        this.messagesPanel.add(this.mainAnnouncements);
+        
+        //Add to the right panel
+        this.rightPanel.add(this.mainServerLog);
+        this.rightPanel.add(this.mainCommands);
+
+        //Add messages panel to the left of the middle part
+        this.add(this.messagesPanel, BorderLayout.WEST);
+        //Add right panel to right of the middle part
+        this.add(this.rightPanel, BorderLayout.EAST);
+
+        //Set the default layout of the main display panel
+        this.mainDisplayPanel.setLayout(new GridLayout(3, 1));
 
         //Add the main panel to the frame
         this.add(this.mainDisplayPanel, BorderLayout.CENTER);
 
         //Initialize the bottom panel
         this.bottomPanel = new JPanel();
-        this.buttonPanel = new JPanel();
+        this.sendPanel = new JPanel();
         this.commandline = new JTextField();
         this.sendButton = new JButton("Send");
 
@@ -118,9 +191,9 @@ public class View extends JFrame {
         this.bottomPanel.setLayout(new GridLayout(1, 2));
 
         //Add the elements to the bottom panel
-        this.buttonPanel.add(this.sendButton);
+        this.sendPanel.add(this.sendButton);
         this.bottomPanel.add(this.commandline);
-        this.bottomPanel.add(this.buttonPanel);
+        this.bottomPanel.add(this.sendPanel);
 
         //Add the bottom panel to the frame
         this.add(this.bottomPanel, BorderLayout.SOUTH);
